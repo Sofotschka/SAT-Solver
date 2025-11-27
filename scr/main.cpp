1dd6baaa7865f2eda0c90b40d99d536b19168d71
Sofie Halbedl <sfhl1081@hochschule-trier.de>
2025-11-27 21:20:47 +0100

    first blueprint of the class and main-method

---
+++ scr/main.cpp
@@ -0,0 +1,27 @@
+#include <unordered_set>
+
+
+class SATSolver{
+
+    public:
+    void initialize(){
+
+    }
+    void solve(){
+
+    }
+    private: 
+    std::unordered_set<int> formula;
+
+
+};
+
+
+
+
+int main(){
+    SATSolver solver;
+    solver.initialize();
+    solver.solve();
+    return 0;
+}
