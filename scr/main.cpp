db50cf13f772dde3911b12b76b91846a8c8c85dd
Sofie Halbedl <sfhl1081@hochschule-trier.de>
2026-01-12 17:49:16 +0100

    reading out the cnf- input file

--- scr/main.cpp
+++ scr/main.cpp
@@ -1,17 +1,44 @@
 #include <unordered_set>
+#include <set>
+#include <iostream>
+#include <string>
 
-
+using namespace std;
 class SATSolver{
 
+    
     public:
+
+    //initialize the solver by reading input
     void initialize(){
+        char c;
+        string s;
+        while(true){
+            cin >> c;
+            if(c == 'c'){ // The character c is a comment in the cnf format
+                getline(cin, s); // puts the rest of the line in s
+            }
+            else{
+                cin >> s;
+                break; // avoids infinite loop
+            }
+        }
+
+        // Read the clauses
+        cin >> clauses_count;
+
+        for(int i = 0; i < clauses_count; i++){
+
+        }
 
     }
     void solve(){
 
     }
     private: 
+    int clauses_count;
     std::unordered_set<int> formula;
+    set<string> clauses;
 
 
 };
